e190af60639a294ebd984b6ccdb5229818d8b615
ShowKin <3064219762@qq.com>
2026-04-08 10:28:08 +0800

    add

--- work/his/js/111.js
+++ work/his/js/111.js
@@ -1,5 +1,7 @@
 var Menu = document.querySelectorAll('.Menu');
-var subMenuItems = document.querySelectorAll('.subMenu_1 li');
+var subMenuItems_1 = document.querySelectorAll('.subMenu_1 li');
+var subMenuItems_2 = document.querySelectorAll('.subMenu_2 li');
+var subMenuItems_3 = document.querySelectorAll('.subMenu_3 li');
 var titleSpans = document.querySelectorAll('.title h6 span');
 
 // 点击一级菜单切换子菜单显示
@@ -12,10 +14,25 @@ Menu.forEach(function(item){
 });
 
 // 点击子菜单项更新title路径
-subMenuItems.forEach(function(item){
+subMenuItems_1.forEach(function(item){
     item.addEventListener('click', function(e){
         e.stopPropagation();
         titleSpans[1].textContent = this.textContent;
         
     })
 });
+subMenuItems_2.forEach(function(item){
+    item.addEventListener('click', function(e){
+        e.stopPropagation();
+        titleSpans[1].textContent = this.textContent;
+        
+    })
+});
+subMenuItems_3.forEach(function(item){
+    item.addEventListener('click', function(e){
+        e.stopPropagation();
+        titleSpans[1].textContent = this.textContent;
+        
+    })
+});
+
