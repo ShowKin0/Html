1dec061ca4b18e5b4d7dd04bb2ee336eefd42e26
ShowKin <3064219762@qq.com>
2026-04-13 15:21:57 +0800

    add

--- work/his/js/111.js
+++ work/his/js/111.js
@@ -1,15 +1,26 @@
 var Menu = document.querySelectorAll('.Menu');
+var btn = document.querySelectorAll('#btn');
+var content = document.querySelectorAll('.content');
 var subMenuItems_1 = document.querySelectorAll('.subMenu_1 li');
 var subMenuItems_2 = document.querySelectorAll('.subMenu_2 li');
 var subMenuItems_3 = document.querySelectorAll('.subMenu_3 li');
 var titleSpans = document.querySelectorAll('.title h6 span');
 
+var act = 0;
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener('click', function() {
+        content[act].classList.remove('visible');
+        act = i;
+        content[i].classList.add('visible');
+})
+}
 // 点击一级菜单切换子菜单显示
 Menu.forEach(function(item){
     item.addEventListener('click', function(e){
         this.classList.toggle('active');
         titleSpans[0].textContent = this.querySelector('span').textContent;
         titleSpans[1].textContent = '';
+
     })
 });
 
@@ -17,9 +28,9 @@ Menu.forEach(function(item){
 subMenuItems_1.forEach(function(item){
     item.addEventListener('click', function(e){
         e.stopPropagation();
+        this.classList.add('active');
         titleSpans[0].textContent = this.parentElement.parentElement.querySelector('span').textContent;
         titleSpans[1].textContent = this.textContent;
-        
     })
 });
 subMenuItems_2.forEach(function(item){
