c04edc5ac3ea6b2baad4a6237a5e4269a256a431
ShowKin <3064219762@qq.com>
2026-04-13 15:51:42 +0800

    fix

--- work/his/js/111.js
+++ work/his/js/111.js
@@ -7,15 +7,17 @@ var act = 0;
 for(let i = 0; i < nav.length; i++){
     nav[i].addEventListener('click', function(e) {
         e.stopPropagation();
+        // 点击二级菜单项切换标题显示
         this.classList.add('active');
         titleSpans[0].textContent = this.parentElement.parentElement.querySelector('span').textContent;
         titleSpans[1].textContent = this.textContent;
+        // 点击导航切换内容显示
         content[act].classList.remove('visible');
         act = i;
         content[i].classList.add('visible');
 })
 }
-// 点击一级菜单切换子菜单显示
+// 点击一级菜单切换标题显示
 Menu.forEach(function(item){
     item.addEventListener('click', function(e){
         this.classList.toggle('active');
